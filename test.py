d0c7b81683684d22b28f294eed4a85f0c4142c32
Limore <1300803599@qq.com>
2020-08-18 09:18:42 +0800

    test

---
+++ test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np  # numpy也是做数据处理比较常用的库，官方文档：https://numpy.org/
+import tensorflow as tf  # 我们玩神经网络的库，官方网站：https://tensorflow.google.cn/
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
